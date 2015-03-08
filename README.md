4c90029a505439596dd689d0242c9fc48b4412b8
Nic Raboy <nraboy@gmail.com>
2015-03-07 22:20:59 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+| Master                                                                                                                            | Development                                                                                                                            |
+|-----------------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------------------------------------------------------|
+| [![Build Status](https://travis-ci.org/nraboy/ng-imgur.svg?branch=master)](https://travis-ci.org/nraboy/ng-imgur) | [![Build Status](https://travis-ci.org/nraboy/ng-imgur.svg?branch=development)](https://travis-ci.org/nraboy/ng-imgur) |
+
+ngImgur
+==============================
+
+ngImgure is an AngularJS library for making RESTful requests to Imgur.
+
+
+Requirements
+-------------
+
+* AngularJS 1.3.x
+
+
+Using ngImgur In Your Project
+-------------
+
+
+Version History
+-------------
+
+[View CHANGELOG](CHANGELOG.md)
+
+
+Have a question or found a bug (compliments work too)?
+-------------
+
+Tweet me on Twitter - [@nraboy](https://www.twitter.com/nraboy)
+
+
+Resources
+-------------
+
+AngularJS - [http://www.angularjs.org](http://www.angularjs.org)
+
+Nic Raboy's Code Blog - [https://blog.nraboy.com](https://blog.nraboy.com)
---
+++ ng-imgur.js
@@ -0,0 +1,104 @@
+(function(){
+
+    angular.module("ngImgur", []).factory("$imgur", ["$q", "$http", function ($q, $http) {
+
+        // Authorization: Bearer ACCESS_TOKEN
+
+        var imgur = function(accessToken) {
+            this.accessToken = accessToken;
+            this.apiBase = "https://api.imgur.com/3";
+        };
+
+        imgur.prototype = {
+
+            /*
+             * Request standard user information. If you need the username for the account that is logged in, it is returned in the request for an access token.
+             */
+            getAccount: function(username) {
+                var deferred = $q.defer();
+                $http({
+                    method: "GET",
+                    url: this.apiBase + "/account/" + username,
+                    headers: {
+                        "Authorization": "Bearer " + this.accessToken
+                    }
+                })
+                .success(function(result) {
+                    deferred.resolve(result);
+                })
+                .error(function(error) {
+                    deferred.reject(error);
+                });
+                return deferred.promise;
+            },
+
+            /*
+             * Return the images the user has favorited in the gallery.
+             */
+            getAccountGalleryFavorites: function(username) {
+                var deferred = $q.defer();
+                $http({
+                    method: "GET",
+                    url: this.apiBase + "/account/" + username + "/gallery_favorites",
+                    headers: {
+                        "Authorization": "Bearer " + this.accessToken
+                    }
+                })
+                .success(function(result) {
+                    deferred.resolve(result);
+                })
+                .error(function(error) {
+                    deferred.reject(error);
+                });
+                return deferred.promise;
+            },
+
+            /*
+             * Returns the users favorited images, only accessible if you're logged in as the user.
+             */
+            getAccountFavorites: function(username) {
+                var deferred = $q.defer();
+                $http({
+                    method: "GET",
+                    url: this.apiBase + "/account/" + username + "/favorites",
+                    headers: {
+                        "Authorization": "Bearer " + this.accessToken
+                    }
+                })
+                .success(function(result) {
+                    deferred.resolve(result);
+                })
+                .error(function(error) {
+                    deferred.reject(error);
+                });
+                return deferred.promise;
+            },
+
+            /*
+             * Return the images a user has submitted to the gallery
+             */
+            getAccountSubmissions: function(username) {
+                var deferred = $q.defer();
+                $http({
+                    method: "GET",
+                    url: this.apiBase + "/account/" + username + "/submissions",
+                    headers: {
+                        "Authorization": "Bearer " + this.accessToken
+                    }
+                })
+                .success(function(result) {
+                    deferred.resolve(result);
+                })
+                .error(function(error) {
+                    deferred.reject(error);
+                });
+                return deferred.promise;
+            }
+
+        };
+
+        return imgur;
+
+    }]);
+
+})();
